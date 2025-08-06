a1da389c259f024c4ed156a07b62453682c9b3e2
biswajit555gravity <biplabi@gravityer.com>
2025-08-06 17:46:47 +0530

    first project push

---
+++ Spring_Hello_world/src/main/java/com/gravity/SpringHelloWorldApplication.java
@@ -0,0 +1,14 @@
+package com.gravity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelloWorldApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
